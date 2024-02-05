d9c668f291630e9fc463edd388954f9a5efcc465
harrypango <harry.pango@gmail.com>
2024-02-05 01:37:05 +0100

    added projects

--- src/components/projects/reactProjects.js
+++ src/components/projects/reactProjects.js
@@ -1,14 +1,15 @@
 import modernai from "../../assets/modernai.jpg";
 import alter from "../../assets/alter.jpg";
+import musify from "../../assets/musify.jpg";
 
 export const reactProjects = [
   {
     key: 0,
-    title: "Modern AI",
-    type: "Modern App",
-    src: modernai,
-    tag: "Completed",
-    href: "https://modern-ai-hp.netlify.app/",
+    title: "Musify",
+    type: "Spotify-like App",
+    src: musify,
+    tag: "In Progress",
+    href: "https://musify-hp.netlify.app/",
   },
   {
     key: 1,
@@ -17,5 +18,13 @@ export const reactProjects = [
     src: alter,
     tag: "In Progress",
     href: "https://alter-architects.com/",
+  },
+  {
+    key: 2,
+    title: "Modern AI",
+    type: "Modern App",
+    src: modernai,
+    tag: "Completed",
+    href: "https://modern-ai-hp.netlify.app/",
   },
 ];
